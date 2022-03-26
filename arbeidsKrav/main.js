cbae6f0b98e3e8e9e704333a5a14aebcef3838e7
Edward Dynestol <edward.dynestol@gmail.com>
2022-03-26 12:18:58 +0100

    går bedre nå

--- arbeidsKrav/main.js
+++ arbeidsKrav/main.js
@@ -1,2 +1,27 @@
-let newFoodList = document.getElementById("newfoodtxt");
-console.log(3);
+let boxContainer = document.getElementById ("food-list1")
+let foodList = document.getElementById ("list-txt")
+
+listArray = [];
+
+function addFood () {
+    
+    let foodNameInput = document.getElementById("food-name-input").value;
+    let foodPriceInput = document.getElementById("food-price-input").value;
+
+    listArray.push({
+        name: foodNameInput,
+        price: foodPriceInput,
+    });
+
+    listFoods();
+}
+
+function listFoods() {
+    foodList.innerHTML = "";
+    for (let i = 0; i < listArray.length; i++){
+        foodList.innerHTML += `<li><h1>${listArray[i].name}</h1>
+        <h2>${listArray[i].price}</h2>
+        <button id="delete-btn" onclick=""></button></li>`;
+    }
+}
+
